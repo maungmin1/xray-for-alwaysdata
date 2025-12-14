66b6f6dd5cad0557323873d3f0b94795d854cdd0
maungmin1 <68060403+maungmin1@users.noreply.github.com>
2025-12-14 12:38:47 +0630

    Translate comments in web.sh to English

--- web.sh
+++ web.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# 定义 UUID 及 伪装路径,请自行修改.(注意:伪装路径以 / 符号开始,为避免不必要的麻烦,请不要使用特殊符号.)
+# Define the UUID and the spoofing path; please modify them as needed. (Note: The spoofing path begins with the "/" symbol. To avoid unnecessary trouble, please do not use special characters.)
 UUID=${UUID:-'de04add9-5c68-8bab-950c-08cd5320df18'}
 VMESS_WSPATH=${VMESS_WSPATH:-'/vmess'}
 VLESS_WSPATH=${VLESS_WSPATH:-'/vless'}
@@ -207,7 +207,7 @@ rm -f config.json
 base64 -d config > config.json
 rm -f config
 
-# 如果有设置哪吒探针三个变量，会安装。如果不填或者不全，则不会安装
+# If the three variables for the Nezha Probe are set, the installation will proceed. If they are not filled in or are incomplete, the installation will not proceed.
 if [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]]; then
     URL=$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
     wget -t 2 -T 10 -N ${URL}
